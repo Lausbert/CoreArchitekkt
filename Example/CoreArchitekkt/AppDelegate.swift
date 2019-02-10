6fdfbaf4a7302b06e3e52fd686cefd7ee220e83d
Stephan Lerner <stephanlerner@icloud.com>
2019-02-10 13:23:53 +0100

    Initial commit

---
+++ Example/CoreArchitekkt/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CoreArchitekkt
+//
+//  Created by Lausbert on 02/10/2019.
+//  Copyright (c) 2019 Lausbert. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
