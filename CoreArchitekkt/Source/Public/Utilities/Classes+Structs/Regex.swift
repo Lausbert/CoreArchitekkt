ad72cf9093a819212cbfbbf39f38af830c6b0cb1
Stephan Lerner <Stephanlerner@icloud.com>
2020-02-16 21:06:16 +0100

    swiftlint autocorrect

--- CoreArchitekkt/Source/Public/Utilities/Classes+Structs/Regex.swift
+++ CoreArchitekkt/Source/Public/Utilities/Classes+Structs/Regex.swift
@@ -8,35 +8,35 @@
 import Foundation
 
 public struct Regex {
-    
+
     // MARK: - Public -
-    
+
     public struct CombinedResult {
         let results: [Result]
     }
-    
+
     public struct Result {
         let captureGroup: Int
         let string: String
         let range: Range<String.Index>
     }
-    
+
     public static func getMatchingStrings(for regex: StaticString, text: String, captureGroups: Int...) -> [CombinedResult] {
         let matches = try! getMatches(for: "\(regex)", text: text)
         return matches.map { match in
             getMachingStrings(for: match, text: text, captureGroups: captureGroups)
         }
     }
-    
+
     public static func getMatchingStrings(for regex: String, text: String, captureGroups: Int...) throws -> [CombinedResult] {
         let matches = try getMatches(for: regex, text: text)
         return matches.map { match in
             getMachingStrings(for: match, text: text, captureGroups: captureGroups)
         }
     }
-    
+
     // MARK: - Private -
-    
+
     private static func getMachingStrings(for match: NSTextCheckingResult, text: String, captureGroups: [Int]) -> CombinedResult {
         let results = captureGroups
             .compactMap { captureGroup in
@@ -48,7 +48,7 @@ public struct Regex {
             }
         return CombinedResult(results: results)
     }
-    
+
     private static func getMatches(for regex: String, text: String) throws -> [NSTextCheckingResult] {
         let regex = try NSRegularExpression(pattern: regex, options: [])
         return regex.matches(in: text, options: [], range: NSRange(text.startIndex..., in: text))
