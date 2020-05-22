5f79ee7e0b56ca75754791bbcd934384368a586f
Stephan Lerner <Stephanlerner@icloud.com>
2020-05-22 11:57:52 +0200

    swiftlint autocorrect

--- CoreArchitekkt/Source/Public/Extensions/Optional.swift
+++ CoreArchitekkt/Source/Public/Extensions/Optional.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 public extension Optional where Wrapped == String {
-    
+
     init?(optionalData: Data?, encoding: String.Encoding) {
         if let data = optionalData {
             self = String.init(data: data, encoding: encoding)
@@ -16,5 +16,5 @@ public extension Optional where Wrapped == String {
             self = nil
         }
     }
-    
+
 }
