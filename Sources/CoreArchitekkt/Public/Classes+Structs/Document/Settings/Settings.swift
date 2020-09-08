3e1b297618e1374209edb5fd00e8c0afb031d3c9
Stephan Lerner <Stephanlerner@icloud.com>
2020-09-08 07:59:11 +0200

    wip

--- Sources/CoreArchitekkt/Public/Classes+Structs/Document/Settings/Settings.swift
+++ Sources/CoreArchitekkt/Public/Classes+Structs/Document/Settings/Settings.swift
@@ -7,20 +7,32 @@ public class Settings: Codable {
 
     // MARK: - Public -
 
-    public var settingsGroups: [SettingsGroup] {
-        forceSettingsGroups + visibilitySettingsGroups
-    }
     public var settingsItems: [SettingsItem] {
-        return settingsGroups.flatMap { $0.settingsItems }
+        return (forceSettingsGroups + visibilitySettingsGroups).flatMap { $0.settingsItems }
+    }
+    
+    public init() {
+        // Force
+        let v1 = SettingsValue.range(value: 0.5, minValue: 0, maxValue: 1)
+        let v2 = SettingsValue.range(value: 1, minValue: 0, maxValue: 2)
+        let v3 = SettingsValue.range(value: -1.1, minValue: -2.1, maxValue: -0.1)
+        let v4 = SettingsValue.range(value: 2.3, minValue: 1, maxValue: 3.6)
+        let v5 = SettingsValue.range(value: 4, minValue: 2, maxValue: 6)
+        decayPowerSettingsItem = SettingsItem(name: "Power", value: v1, initialValue: v1)
+        radialGravitationForceOnChildrenMultiplierSettingsItem = SettingsItem(name: "Multiplier", value: v2, initialValue: v2)
+        negativeRadialGravitationalForceOnSiblingsPowerSettingsItem = SettingsItem(name: "Power", value: v3, initialValue: v3)
+        springForceBetweenConnectedNodesPowerSettingsItem = SettingsItem(name: "Power", value: v4, initialValue: v4)
+        areaBasedOnTotalChildrensAreaMultiplierSettingsItem = SettingsItem(name: "Multiplier", value: v5, initialValue: v5)
+        // Visibility
+        unfoldedNodesSettingsGroup = SettingsGroup(name: "Unfolded Nodes", settingsItems: [])
+        hiddenNodesSettingsGroup = SettingsGroup(name: "Hidden Nodes", settingsItems: [])
+        flattendedNodesSettingsGroup = SettingsGroup(name: "Flattened Nodes", settingsItems: [])
+        unfoldedScopesSettingsGroup = SettingsGroup(name: "Unfolded Scopes", settingsItems: [])
+        hiddenScopesSettingsGroup = SettingsGroup(name: "Hidden Scopes", settingsItems: [])
+        flattendedScopesSettingsGroup = SettingsGroup(name: "Flattened Scopes", settingsItems: [])
     }
 
     // MARK: Force
-
-    public let decayPowerSettingsItem: SettingsItem
-    public let radialGravitationForceOnChildrenMultiplierSettingsItem: SettingsItem
-    public let negativeRadialGravitationalForceOnSiblingsPowerSettingsItem: SettingsItem
-    public let springForceBetweenConnectedNodesPowerSettingsItem: SettingsItem
-    public let areaBasedOnTotalChildrensAreaMultiplierSettingsItem: SettingsItem
 
     public var decayPower: Double {
         if case let .range(value, _, _) = decayPowerSettingsItem.value {
@@ -65,32 +77,15 @@ public class Settings: Codable {
 
     public lazy var forceSettingsGroups: [SettingsGroup] = {
         return [
-            SettingsGroup(name: "Friction", settingsItems: [
-                decayPowerSettingsItem
-                ]),
-            SettingsGroup(name: "Radial Gravitational Force On Children", settingsItems: [
-                radialGravitationForceOnChildrenMultiplierSettingsItem
-                ]),
-            SettingsGroup(name: "Negative Radial Gravitational Force On Siblings", settingsItems: [
-                negativeRadialGravitationalForceOnSiblingsPowerSettingsItem
-                ]),
-            SettingsGroup(name: "Spring Force Between Connected Nodes", settingsItems: [
-                springForceBetweenConnectedNodesPowerSettingsItem
-                ]),
-            SettingsGroup(name: "Area Based On Total Childrens Area", settingsItems: [
-                areaBasedOnTotalChildrensAreaMultiplierSettingsItem
-                ])
+            decayPowerSettingsGroup,
+            radialGravitationForceOnChildrenMultiplierSettingsGroup,
+            negativeRadialGravitationalForceOnSiblingsPowerSettingsGroup,
+            springForceBetweenConnectedNodesPowerSettingsGroup,
+            areaBasedOnTotalChildrensAreaMultiplierSettingsGroup
         ]
     }()
 
     // MARK: Visibility
-
-    public let unfoldedNodesSettingsGroup: SettingsGroup
-    public let hiddenNodesSettingsGroup: SettingsGroup
-    public let flattendedNodesSettingsGroup: SettingsGroup
-    public let unfoldedScopesSettingsGroup: SettingsGroup
-    public let hiddenScopesSettingsGroup: SettingsGroup
-    public let flattendedScopesSettingsGroup: SettingsGroup
 
     public lazy var visibilitySettingsGroups: [SettingsGroup] = {
         return [
@@ -104,26 +99,53 @@ public class Settings: Codable {
     }()
 
     // MARK: - Internal -
-
-    init() {
-        // Force
-        let v1 = SettingsValue.range(value: 0.5, minValue: 0, maxValue: 1)
-        let v2 = SettingsValue.range(value: 1, minValue: 0, maxValue: 2)
-        let v3 = SettingsValue.range(value: -1.1, minValue: -2.1, maxValue: -0.1)
-        let v4 = SettingsValue.range(value: 2.3, minValue: 1, maxValue: 3.6)
-        let v5 = SettingsValue.range(value: 4, minValue: 2, maxValue: 6)
-        decayPowerSettingsItem = SettingsItem(name: "Power", value: v1, initialValue: v1)
-        radialGravitationForceOnChildrenMultiplierSettingsItem = SettingsItem(name: "Multiplier", value: v2, initialValue: v2)
-        negativeRadialGravitationalForceOnSiblingsPowerSettingsItem = SettingsItem(name: "Power", value: v3, initialValue: v3)
-        springForceBetweenConnectedNodesPowerSettingsItem = SettingsItem(name: "Power", value: v4, initialValue: v4)
-        areaBasedOnTotalChildrensAreaMultiplierSettingsItem = SettingsItem(name: "Multiplier", value: v5, initialValue: v5)
-        // Visibility
-        unfoldedNodesSettingsGroup = SettingsGroup(name: "Unfolded Nodes", settingsItems: [])
-        hiddenNodesSettingsGroup = SettingsGroup(name: "Hidden Nodes", settingsItems: [])
-        flattendedNodesSettingsGroup = SettingsGroup(name: "Flattened Nodes", settingsItems: [])
-        unfoldedScopesSettingsGroup = SettingsGroup(name: "Unfolded Scopes", settingsItems: [])
-        hiddenScopesSettingsGroup = SettingsGroup(name: "Hidden Scopes", settingsItems: [])
-        flattendedScopesSettingsGroup = SettingsGroup(name: "Flattened Scopes", settingsItems: [])
-    }
+    
+    // MARK: Force
+    
+    lazy var decayPowerSettingsGroup = SettingsGroup(
+        name: "Friction",
+        settingsItems: [
+            decayPowerSettingsItem
+        ]
+    )
+    lazy var radialGravitationForceOnChildrenMultiplierSettingsGroup = SettingsGroup(
+        name: "Radial Gravitational Force On Children",
+        settingsItems: [
+            radialGravitationForceOnChildrenMultiplierSettingsItem
+        ]
+    )
+    lazy var negativeRadialGravitationalForceOnSiblingsPowerSettingsGroup = SettingsGroup(
+        name: "Negative Radial Gravitational Force On Siblings",
+        settingsItems: [
+            negativeRadialGravitationalForceOnSiblingsPowerSettingsItem
+        ]
+    )
+    lazy var springForceBetweenConnectedNodesPowerSettingsGroup = SettingsGroup(
+        name: "Spring Force Between Connected Nodes",
+        settingsItems: [
+            springForceBetweenConnectedNodesPowerSettingsItem
+        ]
+    )
+    lazy var areaBasedOnTotalChildrensAreaMultiplierSettingsGroup = SettingsGroup(
+        name: "Area Based On Total Childrens Area",
+        settingsItems: [
+            areaBasedOnTotalChildrensAreaMultiplierSettingsItem
+        ]
+    )
+    
+    let decayPowerSettingsItem: SettingsItem
+    let radialGravitationForceOnChildrenMultiplierSettingsItem: SettingsItem
+    let negativeRadialGravitationalForceOnSiblingsPowerSettingsItem: SettingsItem
+    let springForceBetweenConnectedNodesPowerSettingsItem: SettingsItem
+    let areaBasedOnTotalChildrensAreaMultiplierSettingsItem: SettingsItem
+    
+    // MARK: Visibility
+    
+    let unfoldedNodesSettingsGroup: SettingsGroup
+    let hiddenNodesSettingsGroup: SettingsGroup
+    let flattendedNodesSettingsGroup: SettingsGroup
+    let unfoldedScopesSettingsGroup: SettingsGroup
+    let hiddenScopesSettingsGroup: SettingsGroup
+    let flattendedScopesSettingsGroup: SettingsGroup
 
 }
