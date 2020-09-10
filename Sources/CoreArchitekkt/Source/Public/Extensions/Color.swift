560e4f409dcd253b5c38e4e38367733ac72729b5
Stephan Lerner <Stephanlerner@icloud.com>
2020-09-10 22:16:35 +0200

    wip

--- Sources/CoreArchitekkt/Source/Public/Extensions/Color.swift
+++ Sources/CoreArchitekkt/Source/Public/Extensions/Color.swift
@@ -1,9 +1,12 @@
 // Copyright © 2020 Stephan Lerner. All rights reserved.
 
 import SwiftUI
+import AppKit
 
 public extension Color {
     
     static let separatorColor = Color("separatorColor", bundle: Bundle.module)
+    static let controlColor = Color(NSColor.controlColor)
+    static let controlAccentColor = Color(NSColor.controlAccentColor)
     
 }
